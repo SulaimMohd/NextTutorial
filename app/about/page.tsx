f4b3dad8687dee894cc141cda6c53868791392b9
Sulaim <sulaimmohd77@gmail.com>
2024-08-30 19:59:59 +0530

    Link

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Link from 'next/link'
 export default function page() {
   return (
     <div>
       <h1>This is the about page</h1>
+      <Link href="/">Link to the home</Link>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main>
       <h1>Hello world</h1>
+      <Link href="/about">Link to about</Link>
     </main>
   )
 }
